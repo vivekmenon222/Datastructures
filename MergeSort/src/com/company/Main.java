f2c384955cd1d8e12ed394a740102d1615b8781d
Vivek Menon <vivekmenon222@gmail.com>
2016-08-07 20:52:02 -0400

    Basic skeleton

--- MergeSort/src/com/company/Main.java
+++ MergeSort/src/com/company/Main.java
@@ -7,10 +7,15 @@ public class Main {
 	// write your code here
         LinkedList linkedList=new LinkedList();
 
-        linkedList.insertFront(4);
+        linkedList.insertFront(7);
+        linkedList.insertFront(6);
+        linkedList.insertFront(8);
+        linkedList.insertFront(19);
         linkedList.insertFront(2);
-        linkedList.insertFront(3);
-        linkedList.insertFront(1);
+        linkedList.insertFront(15);
+        linkedList.insertFront(9);
+        linkedList.insertFront(5);
+        linkedList.insertFront(22);
         System.out.println("Before Sort");
 
         for(ListNode listNode:linkedList)
--- MergeSort/src/com/company/MergeSort.java
+++ MergeSort/src/com/company/MergeSort.java
@@ -8,34 +8,41 @@ public class MergeSort {
 
     public void Sort(LinkedList linkedList) {
         int totalItems = linkedList.getCount();
-        int secondHalfBegin =1+(int) (totalItems / 2);
+        int secondHalfBegin = 1 + (int) (totalItems / 2);
         sort(1, secondHalfBegin - 1);
         sort(secondHalfBegin, totalItems);
     }
 
-    private void sort(int leftListBegin, int rightListBegin) {
-        if ((rightListBegin - leftListBegin) > 1) {
-            int totalItems = (rightListBegin - leftListBegin) + 1;
-            int secondHalfBegin = leftListBegin + (int) (totalItems / 2);
+    private void sort(int leftIndex, int rightIndex) {
+        if ((rightIndex - leftIndex) > 1) {
+            int totalItems = (rightIndex - leftIndex) + 1;
+            int secondHalfBegin = leftIndex + (int) (totalItems / 2);
+            int leftEnd = secondHalfBegin - 1;
+            if (leftEnd > leftIndex) {
+                //for 3 items this could be 1 item long
+                //don't call sort for that
+                sort(leftIndex, leftEnd);
+            }
 
-            sort(leftListBegin, secondHalfBegin - 1);
-            sort(secondHalfBegin, totalItems);
-            merge(leftListBegin, secondHalfBegin - 1, secondHalfBegin, totalItems);
-        }
-        else
-        {
+            sort(secondHalfBegin, rightIndex);
+            merge(leftIndex, secondHalfBegin - 1, secondHalfBegin, rightIndex);
+        } else {
             System.out.println("--------------");
             System.out.println("reached down to single nodes");
-            System.out.println(String.format("merge %s and %s",leftListBegin,rightListBegin));
+            System.out.println(String.format("merge %s and %s", leftIndex, rightIndex));
             System.out.println("--------------");
         }
     }
 
     private void merge(int leftBegin, int leftEnd, int rightBegin, int rightEnd) {
+        //left begin and end can be same for 3 items. Account for that
+        System.out.println("--------------");
+        System.out.println("Merging lists");
         System.out.println("leftBegin:" + leftBegin);
         System.out.println("leftEnd:" + leftEnd);
         System.out.println("rightBegin:" + rightBegin);
         System.out.println("rightEnd:" + rightEnd);
+        System.out.println("--------------");
     }
 
 }
